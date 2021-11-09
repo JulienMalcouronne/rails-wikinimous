9e3a62f4e14d7d14d953267fd65a09d703d88cf8
Julien Malcouronne <malcouronnejulien@gmail.com>
2021-11-09 15:30:21 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'start seeding'
+
+10.times do
+  article  = Article.new(
+    title: Faker::Name.unique.name,
+    content: Faker::Artist.name.to_s
+  )
+  article.save!
+end
+
+puts 'seeding end...'
